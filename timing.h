e7ee710ee7c499fa90e3a49341a128e60375caab
Anthony Sayre <anthonysayre@icloud.com>
2024-03-27 22:36:42 -0700

    Cleaned up code, timing in microseconds

--- timing.h
+++ timing.h
@@ -5,18 +5,6 @@
 #include <chrono>
 using namespace std;
 
-/*
-template <typename Func>
-long long timeSort(Func sortFunction, const vector<int>& data) {
-    vector<int> dataCopy = data;
-    
-    auto start = chrono::high_resolution_clock::now();
-    sortFunction(dataCopy);  // Sort the copy
-    auto end = chrono::high_resolution_clock::now();
-    
-    return chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-} 
-*/
 template <typename Func>
 long long timeSort(Func sortFunction, vector<int> data){
     auto start = chrono::high_resolution_clock::now();
@@ -24,18 +12,3 @@ long long timeSort(Func sortFunction, vector<int> data){
     auto end = chrono::high_resolution_clock::now();
     return chrono::duration_cast<std::chrono::microseconds>(end - start).count();
 }
-
-
-
-
-/*
-// Declaration of the sort functions
-void insertionSort(vector<int> &vec);
-void selectionSort(vector<int> &vec);
-void bubbleSort(vector<int> &vec);
-void mergeSort(vector<int> &vec);
-void quickSort(vector<int> &vec);
-void heapSort(vector<int> &vec);
-void countingSort(vector<int> &vec);
-void radixSort(vector<int> &vec);
-*/
